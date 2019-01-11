85f0e8a5ed043394ccc59edeb22518770920c714
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-10 19:20:17 -0500

    Update CookieWrongfullyMadeException.php

--- src/Oven/Exception/CookieWrongfullyMadeException.php
+++ src/Oven/Exception/CookieWrongfullyMadeException.php
@@ -1,1 +1,10 @@
+<?php
+declare(strict_types=1);
+/**
+ * Baker - A simple PHP extension for basic cookie functions.
+ */
+namespace Bakery\Oven\Exception;
 
+use Exception;
+
+class CookieWrongfullyMadeException extends Exception implements ExceptionInterface { }
