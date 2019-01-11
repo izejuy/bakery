9bd0855163c7c1ed283e1cb02e5841240c8aeb96
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-10 19:24:00 -0500

    Fix CS Standards.

--- src/Oven/Exception/InspectorDisagreesException.php
+++ src/Oven/Exception/InspectorDisagreesException.php
@@ -1,1 +1,13 @@
+<?php
+declare(strict_types=1);
+/**
+ * Baker - A simple PHP extension for basic cookie functions.
+ */
 
+namespace Bakery\Oven\Exception;
+
+use Exception;
+
+class InspectorDisagreesException extends Exception implements ExceptionInterface
+{
+}
