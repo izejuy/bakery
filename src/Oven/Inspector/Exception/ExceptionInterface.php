6847d7fdbfbffd51dd11f9fa5706873a0cbd192a
izejuy <46389306+izejuy@users.noreply.github.com>
2019-01-10 19:27:01 -0500

    Fix CS Standards.

--- src/Oven/Inspector/Exception/ExceptionInterface.php
+++ src/Oven/Inspector/Exception/ExceptionInterface.php
@@ -1,1 +1,13 @@
+<?php
+declare(strict_types=1);
+/**
+ * Baker - A simple PHP extension for basic cookie functions.
+ */
 
+namespace Bakery\Oven\Exception;
+
+use Throwable;
+
+interface ExceptionInterface extends Throwable
+{
+}
